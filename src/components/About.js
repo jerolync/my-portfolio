92f113237779bb2a75f300b12f32ced9036c0b3f
jerolync <jerolysimonc@gmail.com>
2024-11-15 20:32:56 -0500

    Updated project section

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>This is a little bit about myself...</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Me</h2>
+      <p>Feel free to reach out to me via email or LinkedIn!</p>
+    </div>
+  );
+}
+
+export default Contact;
